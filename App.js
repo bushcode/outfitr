e1a5280857cffbd2a103de863aadca5945e5da69
Folarin Farinto <folysmith@gmail.com>
2020-08-28 09:24:08 +0100

    initial commit :rocket

--- App.js
+++ App.js
@@ -1,21 +1,18 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+
+const AuthenticationStack = createStackNavigator();
+const AuthenticationNavigator = () => {
+  <AuthenticationStack.Navigator>
+    <AuthenticationStack.Screen name="Onboarding" component={Onboarding} />
+  </AuthenticationStack.Navigator>;
+};
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <AuthenticationNavigator />
+    </NavigationContainer>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
